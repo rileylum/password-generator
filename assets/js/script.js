4fca337089b701eb155d5afe1d3a848602533d23
Riley Lum <rileylum@adam.com.au>
2021-06-22 17:09:33 +0930

    add user input for password length

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,5 +9,14 @@ function writePassword() {
   passwordText.value = password;
 }
 
+function generatePassword() {
+  // get password length from user input
+  var passwordLength = parseInt(window.prompt("Set desired password length (between 8 and 128 characters): "));
+  // keep asking for user input whilst input is invalid 
+  while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    passwordLength = parseInt(window.prompt("Set desired password length (between 8 and 128 characters): "));
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
