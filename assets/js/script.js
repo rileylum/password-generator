f9183a60cf2c40cb6dbd7762e6770ea14c3d6b0b
Riley Lum <rileylum@adam.com.au>
2021-06-22 17:50:06 +0930

    add all characters to their arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
-const special = ["!", "\"", "\#"];
+const special = ["!", "\"", "\#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-const lower = ["a", "b", "c"];
-const upper = ["A", "B", "C"];
+const lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Write password to the #password input
 function writePassword() {
